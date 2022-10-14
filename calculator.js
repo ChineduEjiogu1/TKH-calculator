80c1761be0380bc76ddd4f0fd3acb76c0e20c18c
ChineduEjiogu1 <chineduejiogu@gmail.com>
2022-10-14 23:12:54 +0100

    first draft

--- calculator.js
+++ calculator.js
@@ -3,28 +3,28 @@ function addition(num, num2)
     return num + num2
 }
 
-console.log(addition(213489, 48378))
+// console.log(addition(213489, 48378))
 
 function subtraction(num, num2)
 {
     return num - num2;
 }
 
-console.log(subtraction(34200, 4478))
+// console.log(subtraction(34200, 4478))
 
 function multiplication(num, num2)
 {
     return num * num2
 }
 
-console.log(multiplication(3550, 478))
+// console.log(multiplication(3550, 478))
 
 function division(num, num2)
 {
     return num / num2
 }
 
-console.log(division(23993, 388))
+// console.log(division(23993, 388))
 
 function operate(operator, num, num2)
 {
@@ -49,13 +49,14 @@ function operate(operator, num, num2)
 let num = 0
 let num2 = 0
 let operatorButtonVal = ''
+let result = 0
 
 function equalsClicked()
 {
     // saving the number value of the first input to num1
     num = document.querySelector('#num').value
     // parseInt changes num1 data type to number
-    num = praseInt(num)
+    num = parseInt(num)
 
     operatorButtonVal = document.querySelector('#operator').value
     console.log(operatorButtonVal)
@@ -63,14 +64,38 @@ function equalsClicked()
     //next steps 
 
     //create a variable num2
+    num2 = document.querySelector('#num2').value
+    num2 = parseInt(num2)
+
+    operatorButtonVal = document.querySelector('#operator').value
+    console.log(operatorButtonVal)
     // create values console.log
-    //connect math functions
-    //display result on the page using DOM methods
+    console.log(num)
+    console.log(num2)
+
+    // connect math functions
+    // https://www.geeksforgeeks.org/how-to-get-selected-value-in-dropdown-list-using-javascript/
+    // https://stackoverflow.com/questions/52767068/how-do-i-link-my-javascript-function-to-an-html-element
+    if(document.getElementById('operator').options[document.getElementById('operator').selectedIndex].value === "add")
+        result = operate('add', num, num2)
+    else if(document.getElementById('operator').options[document.getElementById('operator').selectedIndex].value === "subtract")
+        result = operate('subtract', num, num2)
+    else if(document.getElementById('operator').options[document.getElementById('operator').selectedIndex].value === "multiply")
+        result = operate('multiply', num, num2)
+    else if(document.getElementById('operator').options[document.getElementById('operator').selectedIndex].value === "divide")
+        result = operate('divide', num, num2)
+    else
+        return 0
+
+    // display result on the page using DOM methods
+    // needs num and num2
+    document.getElementById("result-num").innerHTML = result
+    console.log(result)
 }
 
-console.log(operate('add', 1, 5))
-console.log(operate('subtract', 1, 5))
-console.log(operate('multiply', 3, 5))
-console.log(operate('divide', 28, 5))
+// console.log(operate('add', 1, 5))
+// console.log(operate('subtract', 1, 5))
+// console.log(operate('multiply', 3, 5))
+// console.log(operate('divide', 5, 28))
 
-function operate(num)
+// function operate(num)

d1e1580f174eb841f6248bd9b3209c6ebd0d17a6
ChineduEjiogu1 <chineduejiogu@gmail.com>
2022-10-17 20:07:05 +0100

    Final draft

--- calculator.js
+++ calculator.js
@@ -1,46 +1,38 @@
-function addition(num, num2)
+function add(num, num2)
 {
     return num + num2
 }
 
-// console.log(addition(213489, 48378))
-
-function subtraction(num, num2)
+function subtract(num, num2)
 {
     return num - num2;
 }
 
-// console.log(subtraction(34200, 4478))
-
-function multiplication(num, num2)
+function multiply(num, num2)
 {
     return num * num2
 }
 
-// console.log(multiplication(3550, 478))
-
-function division(num, num2)
+function divide(num, num2)
 {
     return num / num2
 }
-
-// console.log(division(23993, 388))
 
 function operate(operator, num, num2)
 {
     operator = operator.toLowerCase()
 
     if(operator === 'add')
-        return addition(num, num2)
+        return add(num, num2)
     
     else if(operator === 'subtract')
-        return subtraction(num, num2)
+        return subtract(num, num2)
 
     else if(operator === 'multiply')
-        return multiplication(num, num2)
+        return multiply(num, num2)
     
     else if(operator === 'divide')
-        return division(num, num2)
+        return divide(num, num2)
     
     else
         return `Error: operator not recognized`
@@ -55,48 +47,41 @@ function equalsClicked()
 {
     // saving the number value of the first input to num1
     num = document.querySelector('#num').value
-    // parseInt changes num1 data type to number
-    num = parseInt(num)
-
-    operatorButtonVal = document.querySelector('#operator').value
-    console.log(operatorButtonVal)
-
-    //next steps 
-
     //create a variable num2
     num2 = document.querySelector('#num2').value
+    // parseInt changes num1 data type to number
+    num = parseInt(num)
     num2 = parseInt(num2)
 
+    //next steps 
     operatorButtonVal = document.querySelector('#operator').value
     console.log(operatorButtonVal)
-    // create values console.log
-    console.log(num)
-    console.log(num2)
 
+    // create values console.log
+    console.log(typeof num)
+    console.log(typeof num2)
+    
+    // Wren this is the long way
     // https://www.w3schools.com/jsref/prop_select_selectedindex.asp
     // https://www.geeksforgeeks.org/how-to-get-selected-value-in-dropdown-list-using-javascript/
     // https://stackoverflow.com/questions/52767068/how-do-i-link-my-javascript-function-to-an-html-element
-    let mathOperators = document.getElementById('operator')
-    if(mathOperators.options[mathOperators.selectedIndex].value === "add")
-        result = operate('add', num, num2)
-    else if(mathOperators.options[mathOperators.selectedIndex].value === "subtract")
-        result = operate('subtract', num, num2)
-    else if(mathOperators.options[mathOperators.selectedIndex].value === "multiply")
-        result = operate('multiply', num, num2)
-    else if(mathOperators.options[mathOperators.selectedIndex].value === "divide")
-        result = operate('divide', num, num2)
-    else
-        return 0
+    // let mathOperators = document.getElementById('operator')
+    // if(mathOperators.options[mathOperators.selectedIndex].value === "add")
+    //     result = operate('add', num, num2)
+    // else if(mathOperators.options[mathOperators.selectedIndex].value === "subtract")
+    //     result = operate('subtract', num, num2)
+    // else if(mathOperators.options[mathOperators.selectedIndex].value === "multiply")
+    //     result = operate('multiply', num, num2)
+    // else if(mathOperators.options[mathOperators.selectedIndex].value === "divide")
+    //     result = operate('divide', num, num2)
+    // else
+    //     return 0
+
+    // Wren this is the short way.
+
+    result = operate(operatorButtonVal, num, num2)
 
     // display result on the page using DOM methods
-    // needs num and num2
     document.getElementById("result-num").innerHTML = result
     console.log(result)
 }
-
-// console.log(operate('add', 1, 5))
-// console.log(operate('subtract', 1, 5))
-// console.log(operate('multiply', 3, 5))
-// console.log(operate('divide', 5, 28))
-
-// function operate(num)
